660e844844dc81bb3a8c41e08b9fc7365736bc48
SM_artist <48393754+jbrace817@users.noreply.github.com>
2023-06-23 15:48:05 -0400

    All working. Begin Clean up

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ const divs = document.createElement("div");
 const clear = document.getElementById("clear");
 const color = document.getElementById("color");
 const randomBtn = document.getElementById("random");
+const eraserIcon = document.getElementById("eraser");
 let random = 0;
 
 divs.classList.add("square");
@@ -43,34 +44,52 @@ function randomColor() {
   return "rgb(" + color.join(", ") + ")";
 }
 
+//EventListener for Random color toggle switch
 randomBtn.addEventListener("click", () => {
   const squareDivs = document.querySelectorAll(".square");
   const random = document.getElementsByClassName("randomColor");
-  let btnStyle, btnLabel;
-  const randomTxt = document.getElementById("randomTxt");
+  let btnStyle;
   squareDivs.forEach((squarediv) => squarediv.classList.toggle("randomColor"));
 
   if (random.length > 0) {
-    btnStyle = "green";
-    btnLabel = "ON";
+    //Button ON
+    btnStyle = "#2676ed";
     // console.log(random);
+    randomBtn.innerHTML =
+      "<i class='fa fa-toggle-on' style='font-size:36px'></i>";
   } else {
-    btnStyle = "";
-    btnLabel = "OFF";
+    //Button OFF
+    btnStyle = "#d8d5d5";
+    randomBtn.innerHTML =
+      "<i class='fa fa-toggle-off' style='font-size:36px'></i>";
   }
-  randomBtn.style.background = btnStyle;
-  randomTxt.innerText = btnLabel;
+  randomBtn.style.color = btnStyle;
+});
+
+eraser.addEventListener("click", () => {
+  const squareDivs = document.querySelectorAll(".square");
+  const erase = document.getElementsByClassName("eraser");
+  squareDivs.forEach((squarediv) => squarediv.classList.toggle("eraser"));
+  if (erase.length > 0) {
+    eraserIcon.style.color = "#df93c5";
+  } else {
+    eraserIcon.style.color = "#d8d5d5";
+  }
 });
 
 container.addEventListener("mouseover", function (e) {
   let colorPicker = color.value;
   let target = e.target.classList.contains("square");
   let target2 = e.target.classList.contains("randomColor");
+  let erase = e.target.classList.contains("eraser");
   if (target) {
     e.target.style.backgroundColor = colorPicker;
   }
   if (target2) {
     e.target.style.backgroundColor = randomColor();
+  }
+  if (erase) {
+    e.target.style.backgroundColor = "white";
   }
 });
 
